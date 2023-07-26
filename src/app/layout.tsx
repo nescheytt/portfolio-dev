c0cdb0a4aa7e428b841273f6c4f72e9d6f9e032e
Nahuel Scheytt <nahuel.scheytt@gmail.com>
2023-07-26 13:02:33 +0100

    [Feat] share og (#4)
    * feat: improvs
    
    * feat: seen
    
    * feat: revert commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,16 +23,11 @@ export const metadata: Metadata = {
     siteName: 'Nahuel Scheytt | Frontend Developer',
     images: [
       {
-        url: 'img/view-home-og.png',
+        url: 'https://nahuelscheytt.com/src/img/home-preview.jpg',
+        type: 'image/jpg',
         width: 800,
-        height: 600,
-      },
-      {
-        url: 'img/view-home-og-alt.png',
-        width: 1800,
-        height: 1600,
-        alt: 'View site alt',
-      },
+        height: 600
+      }
     ],
     locale: 'en_US',
     type: 'website',
@@ -43,7 +38,7 @@ export const metadata: Metadata = {
     description: 'Nahuel Scheytt is a Frontend Developer yet capable to develop on backend.',
     creator: '@nahueesch',
     images: {
-      url: 'img/view-home-og.png',
+      url: 'https://nahuelscheytt.com/src/img/home-preview.jpg',
       alt: 'Nahuel Scheytt | Frontend Developer Logo',
     },
   },
