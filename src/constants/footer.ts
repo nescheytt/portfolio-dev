6cc88810a77afa7949f45de19aec33b149969d81
nescheytt <nahuel.scheytt@gmail.com>
2023-07-27 16:33:35 +0100

    feat: add fn map to list items footer

---
+++ src/constants/footer.ts
@@ -0,0 +1,14 @@
+export const listItems = [
+  {
+    href: "https://www.linkedin.com/in/nahuel-scheytt/",
+    label: "LinkedIn"
+  },
+  {
+    href: "https://twitter.com/nahueesch",
+    label: "Twitter"
+  },
+  {
+    href: "mailto:nahuel.scheytt@gmail.com",
+    label: "Email"
+  },
+]
