c77558f7089359ec820c630970a1cf11ae1b4319
nescheytt <nahuel.scheytt@gmail.com>
2023-07-28 09:14:33 +0100

    feat: change wordings about comp

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,4 +1,4 @@
-import { useMouseEventsContext } from "@/context/mouseEvents"
+import { useMouseEventsContext } from '@/context/mouseEvents'
 
 const About = () => {
   const mouseEvents = useMouseEventsContext()
@@ -10,10 +10,10 @@ const About = () => {
           I started this passionate adventure in web technologies <span className="text-gray-300">8+ years</span> ago.
         </p>
         <p className="text-gray-500 font-bold text-2xl max-md:text-center md:text-4xl hover:text-gray-300 transition duration-500 hover:scale-105">
-          During all this time my developments were becoming <span className="text-gray-300">pixel-expert</span>, performant, accessible, and <span className="text-gray-300">full-responsive</span> websites/applications.
+          During all this time, my developments have evolved into <span className="text-gray-300">pixel-perfect</span>, performant, accessible, and <span className="text-gray-300">fully responsive</span> websites/applications.
         </p>
         <p className="text-gray-500 font-bold text-2xl max-md:text-center md:text-4xl hover:text-gray-300 transition duration-500 hover:scale-105">
-          {`I've always been enthusiastic about the whole development aspect so I often participate in the `}<span className="text-gray-300">backend.</span>
+          {`I've always been enthusiastic about the entire development process, so I often participate in the `}<span className="text-gray-300">backend development.</span>
         </p>
         <p className="text-gray-500 font-bold text-2xl max-md:text-center md:text-4xl hover:text-gray-300 transition duration-500 hover:scale-105">
           <span className="text-gray-300">I love</span> working with ambitious people and agencies, with beautiful designs and experiences.
