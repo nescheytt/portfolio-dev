47c8462214e80a9a10cdb14cae9deddd2633da30
nescheytt <nahuel.scheytt@gmail.com>
2023-11-15 14:16:59 +0000

    feat: add access to github in footer

--- src/constants/footer.ts
+++ src/constants/footer.ts
@@ -1,14 +1,28 @@
-export const listItems = [
-  {
-    href: "https://www.linkedin.com/in/nahuel-scheytt/",
-    label: "LinkedIn"
-  },
-  {
-    href: "https://twitter.com/nahueesch",
-    label: "Twitter"
-  },
-  {
-    href: "mailto:nahuel.scheytt@gmail.com",
-    label: "Email"
-  },
+type ListItems = {
+	id: number
+	href: string
+	label: string
+}
+
+export const listItems: ListItems[] = [
+	{
+		id: 1,
+		href: 'https://www.linkedin.com/in/nahuel-scheytt/',
+		label: 'LinkedIn'
+	},
+	{
+		id: 2,
+		href: 'https://github.com/nescheytt',
+		label: 'GitHub'
+	},
+	{
+		id: 3,
+		href: 'https://twitter.com/nahueesch',
+		label: 'Twitter'
+	},
+	{
+		id: 4,
+		href: 'mailto:nahuel.scheytt@gmail.com',
+		label: 'Email'
+	}
 ]
