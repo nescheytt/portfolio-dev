b66393732f9d64fe4ad5ff882e7b9a4c4c781c1f
nescheytt <nahuel.scheytt@gmail.com>
2023-11-15 17:34:51 +0000

    chore: update dependencies + update next.js 14

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import "@/styles/globals.css"
 import { Inter } from "next/font/google"
 import { configMetadata } from "@/config/metadata"
+import { configViewport } from "@/config/viewport"
 
 const inter = Inter({ subsets: ["latin"] })
 export const metadata = configMetadata
+export const viewport = configViewport
 
 export default function RootLayout({
   children,
