e3f99c44960da76d8b6a299509b05bf542bc86fd
nescheytt <nahuel.scheytt@gmail.com>
2023-11-15 17:36:15 +0000

    style: prettier

--- src/config/viewport.ts
+++ src/config/viewport.ts
@@ -2,8 +2,8 @@ import type { Viewport } from "next"
 
 export const configViewport: Viewport = {
   themeColor: {
-    media: '(prefers-color-scheme: light)',
-    color: '#ffd7ed'
+    media: "(prefers-color-scheme: light)",
+    color: "#ffd7ed",
   },
   colorScheme: "light",
   width: "device-width",
